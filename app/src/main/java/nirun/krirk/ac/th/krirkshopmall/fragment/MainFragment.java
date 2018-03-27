219deb65c0fa4894dba22abf7320603664e804ce
nirun <ngamkerd@gmail.com>
2018-03-27 11:52:39 +0700

    commit test 27 mar 2018

--- app/src/main/java/nirun/krirk/ac/th/krirkshopmall/fragment/MainFragment.java
+++ app/src/main/java/nirun/krirk/ac/th/krirkshopmall/fragment/MainFragment.java
@@ -7,9 +7,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import nirun.krirk.ac.th.krirkshopmall.R;
+import nirun.krirk.ac.th.krirkshopmall.utility.MyAlert;
 
 /**
  * Created by nirun on 6/3/2561.
@@ -25,8 +28,35 @@ public class MainFragment extends Fragment {
 //        register controller
         registerController();
 
+        // Login Controler
+        loginControler();
+
 
     }//        main method
+
+    private void loginControler() {
+        Button button = getView().findViewById(R.id.btnlogin);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                EditText userEditText = getView().findViewById(R.id.edtuser);
+                EditText passwordEditText = getView().findViewById(R.id.edtpassword);
+
+                String userString = userEditText.getText().toString().trim();
+                String passwordString = passwordEditText.getText().toString().trim();
+
+                if (userString.isEmpty() || passwordString.isEmpty()){
+
+                    MyAlert myAlert = new MyAlert(getActivity());
+                    myAlert.myDialog(getString(R.string.title_have_space),
+                            getString(R.string.message_have_space));
+                }else {
+          //          No space
+                }
+            }
+        });
+    }
 
     private void registerController() {
         TextView textView = getView().findViewById(R.id.txtnewregister);
